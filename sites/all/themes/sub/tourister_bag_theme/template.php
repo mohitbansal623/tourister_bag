07058e1fa6c156d396511eec6ae889d88403989f
mohitbansal623 <mohit.bansal623@gmail.com>
2018-05-05 20:49:04 +0530

    theming setup

--- sites/all/themes/sub/tourister_bag_theme/template.php
+++ sites/all/themes/sub/tourister_bag_theme/template.php
@@ -4,3 +4,13 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+function tourister_bag_theme_preprocess_page(&$variables) {
+  $path = path_to_theme();
+  drupal_add_css($path . '/stylesheets/basic_layout.css');
+}
