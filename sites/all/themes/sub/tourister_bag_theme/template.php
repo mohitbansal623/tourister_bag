6108b156f38e33d7ce1df0e78e08bab73d106ef6
mohitbansal623 <mohit.bansal623@gmail.com>
2018-05-18 00:22:16 +0530

    banner added and libraries, paytm module

--- sites/all/themes/sub/tourister_bag_theme/template.php
+++ sites/all/themes/sub/tourister_bag_theme/template.php
@@ -13,4 +13,9 @@
 function tourister_bag_theme_preprocess_page(&$variables) {
   // $path = path_to_theme();
   // drupal_add_css($path . '/stylesheets/basic_layout.css');
+  $theme_path = drupal_get_path('theme', 'tourister_bag_theme');
+  $slick_lib = libraries_get_path('slick');
+  drupal_add_js($slick_lib .'/slick/slick.min.js');
+  drupal_add_css($slick_lib .'/slick/slick.css');
+  drupal_add_css($slick_lib .'/slick/slick-theme.css');
 }
