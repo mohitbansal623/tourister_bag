a5bdf84366c70b78286bfa2455175e2f55a9d281
mohitbansal623 <mohit.bansal623@gmail.com>
2019-01-13 12:18:12 +0530

    latest code pulled and pushed

--- sites/all/themes/sub/tourister_bag_theme/js/general.js
+++ sites/all/themes/sub/tourister_bag_theme/js/general.js
@@ -34,21 +34,41 @@
     //     }
     //   ]
     // });
-
     var tax = 0;
     var tax_type = '';
     var total_amount = $(".page-user-orders.page-user-orders- .component-type-commerce-price-formatted-amount  td.component-total").text();
     $(".page-user-orders.page-user-orders- tr.all-taxes").each(function( index ) {
-
+         
          tax_type += $( this ).text() + "+";
-         total_tax = $("tr.all-taxes td.component-total").text();
+         total_tax = $("tr.all-taxes td.component-total:first").text();
+         total_tax = total_tax.replace(',','');
+         console.log(total_tax);
+
          tax += parseFloat(total_tax.match(/[\d\.]+/));
+         console.log(tax);
 
-    });
-    tax_type = tax_type.slice(0,-1);
-    var taxes = '<div class="bill-summary container"><div class="total-amount-with-GST"> Total Amount including GST: ' + total_amount +'</div><div class="total-taxes-type">' + tax_type + '= ₹' +  tax + '</div></div>';
-    $(".page-user-orders.page-user-orders- .bank-details").before(taxes);
+});
+tax_type = tax_type.slice(0,-1);
+var amount = $(".page-user-orders.page-user-orders- .view-commerce-line-item-table td.views-field-commerce-total").text();
+var taxes = '<div class="bill-summary container"><div class="total-amount-with-GST"> Total Amount: ' + amount +  '+ GST:  ₹' + tax + ' = '+ total_amount +'</div><div class="total-taxes-type">' + tax_type + '= ₹' +  tax + '</div></div>';
 
+$(".page-user-orders.page-user-orders- .bank-details").before(taxes);
+$(".page-user-orders.page-user-orders- .field-name-commerce-customer-billing").insertBefore(".page-user-orders.page-user-orders- .view-header .order-date-time.invoice-number");
+
+$(".page-user-orders.page-user-orders- .view-header .gstinno").insertAfter(".page-user-orders.page-user-orders- .field-name-commerce-customer-billing .field-name-field-phone-no");
+
+$(".page-user-orders.page-user-orders- .view-header .narration-section").insertAfter(".page-user-orders.page-user-orders- .bill-summary");
+
+
+
+
+$("body .print-button .print").click(function() {
+   window.print(); 
+});
+
+$(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='commerce_discount']").remove();
+$(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='shipping']").remove();
+$(".page-node-add-advance-booking .field-name-field-select-product .form-select option[value='product_discount']").remove();
 
 
   });
--- sites/all/themes/sub/tourister_bag_theme/templates/page.tpl.php
+++ sites/all/themes/sub/tourister_bag_theme/templates/page.tpl.php
@@ -92,9 +92,7 @@ global $user;
         </a>
       <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-        <h3><a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php //print $site_name; ?>Jn<span>bag</span></a></h3>
-      <?php endif; ?>
+      
 
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
